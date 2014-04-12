9ec8f1b307e0f944068c05f7e3625de1fd9cd221
Reinier <reinier@TheMatrix.(none)>
2014-04-12 02:49:37 -0400

    update fro heroku

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
-
 /**
  * Module dependencies.
  */
-
 var express = require('express');
 var routes = require('./controller');
 var http = require('http');
 var path = require('path');
-
-
 var app = express();
-
-
-
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.use(express.logger('dev'));
@@ -21,28 +14,25 @@ app.use(express.bodyParser());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
-
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
-
 app.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    next();
 });
-
-app.get('/',function (req,res) { res.sendfile("README.md");});
+app.get('/', function(req, res) {
+    res.set('Content-Type', 'text/html');
+    res.sendfile("README.md");
+});
 app.get('/blogs', routes.list);
-app.get('/blog/:id',routes.blog);
-app.post('/blog',routes.add);
-app.delete('/blog/:id',routes.delete);
-app.put('/blog/:id',routes.update);
-
-
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+app.get('/blog/:id', routes.blog);
+app.post('/blog', routes.add);
+app.delete('/blog/:id', routes.delete);
+app.put('/blog/:id', routes.update);
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
 });
